5a7f140b1d7df7576b85d56665cfb37041ac3c4e
Orijeet Shankar Mukherjee <88101748+orijeet100@users.noreply.github.com>
2025-09-12 18:49:35 -0400

    Create canConstruct.py

---
+++ canConstruct.py
@@ -0,0 +1,57 @@
+# Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+# Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+# Example 1:
+
+# Input: ransomNote = "a", magazine = "b"
+# Output: false
+# Example 2:
+
+# Input: ransomNote = "aa", magazine = "ab"
+# Output: false
+# Example 3:
+
+# Input: ransomNote = "aa", magazine = "aab"
+# Output: true
+ 
+
+# Constraints:
+
+# 1 <= ransomNote.length, magazine.length <= 105
+# ransomNote and magazine consist of lowercase English letters.
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+
+        letter_count={}
+
+        for char in magazine:
+            if char in letter_count:
+                letter_count[char]+=1
+            
+            else:
+                letter_count[char]=1
+
+        
+        for char in ransomNote:
+            if char in letter_count:
+                if letter_count[char]==0:
+                    return False
+
+                else:
+                    letter_count[char]-=1
+            
+            else:
+                return False
+
+        return True
+            
+        
