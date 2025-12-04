bcbe549332ae096626c3d4f7151504ff45fd35c6
Orijeet Shankar Mukherjee <88101748+orijeet100@users.noreply.github.com>
2025-12-03 19:53:45 -0500

    Create areOccurrencesEqual.py

---
+++ areOccurrencesEqual.py
@@ -0,0 +1,42 @@
+# Given a string s, return true if s is a good string, or false otherwise.
+
+# A string s is good if all the characters that appear in s have the same number of occurrences (i.e., the same frequency).
+
+ 
+
+# Example 1:
+
+# Input: s = "abacbc"
+# Output: true
+# Explanation: The characters that appear in s are 'a', 'b', and 'c'. All characters occur 2 times in s.
+# Example 2:
+
+# Input: s = "aaabb"
+# Output: false
+# Explanation: The characters that appear in s are 'a' and 'b'.
+# 'a' occurs 3 times while 'b' occurs 2 times, which is not the same number of times.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 1000
+# s consists of lowercase English letters.
+
+
+class Solution(object):
+    def areOccurrencesEqual(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        mapping={}
+
+        for char in s:
+            if char in mapping:
+                mapping[char]+=1
+
+            else:
+                mapping[char]=1
+
+        
+        return len(set(mapping.values()))==1        
